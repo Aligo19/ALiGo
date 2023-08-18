e2b86d9fd1207ea4cca3bb1482fc8b6496ab8d28
Hugo Goorickx <hugo@goorickx.be>
2023-08-18 18:13:21 +0200

    On oublie pas sudo

--- srcs/api/src/main.ts
+++ srcs/api/src/main.ts
@@ -4,8 +4,10 @@ import { seedConvs } from './conv/conv.seed';
 import { seedMatches } from './match/match.seed';
 import { seedUsers } from './user/user.seed';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await seedUsers();
   await seedMatches();
